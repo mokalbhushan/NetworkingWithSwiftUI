9e1c6f3b771a5b8961f2851a81481d9d7d307887
Bhushan Mokal <bhushan.mokal@neosoftmail.com>
2023-06-06 14:47:49 +0530

    Initial Commit

---
+++ NetworkingWithSwiftUI/NetworkingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWithSwiftUIApp.swift
+//  NetworkingWithSwiftUI
+//
+//  Created by Bhushan Mokal on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
